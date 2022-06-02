b1644f473acd8c20cb1e76d5590b48f826d08625
Ignacio <ignaciodieguez@hotmail.com>
2022-06-02 20:29:56 -0300

    error con el json

--- back.js
+++ back.js
@@ -1,40 +1,83 @@
-class Persona {
-    constructor(nombre, apellido, libros, mascotas){
-        this.nombre = nombre
-        this.apellido = apellido
-        this.libros = libros
-        this.mascotas = mascotas
-    }
+//EJERCICIO 1
 
-    getFullName() {
-        console.log(this.nombre +" "+ this.apellido);
-    }
+// class Persona {
+//     constructor(nombre, apellido, libros, mascotas){
+//         this.nombre = nombre
+//         this.apellido = apellido
+//         this.libros = libros
+//         this.mascotas = mascotas
+//     }
 
-    countMascotas () {
-        console.log(this.mascotas.length);
-    }
+//     getFullName() {
+//         console.log(this.nombre +" "+ this.apellido);
+//     }
 
-    addbookNames(nombre, autor) {
-        this.libros.push({nombre: nombre, autor: autor})
-        console.log(this.libros);
-    }
+//     countMascotas () {
+//         console.log(this.mascotas.length);
+//     }
 
-    getBooksName () {
-        let libros = this.libros
-        let nombreLibro = libros.map((libro)=> libro.nombre);
-        console.log(nombreLibro);
-        return nombreLibro
+//     addbookNames(nombre, autor) {
+//         this.libros.push({nombre: nombre, autor: autor})
+//         console.log(this.libros);
+//     }
+
+//     getBooksName () {
+//         let libros = this.libros
+//         let nombreLibro = libros.map((libro)=> libro.nombre);
+//         console.log(nombreLibro);
+//         return nombreLibro
         
+//     }
+// }
+
+// const nacho = new Persona("Ignacio" , "dieguez" ,[], ["cherry" , "ginger"])
+
+// console.log(nacho);
+// nacho.getFullName()
+// nacho.countMascotas
+// console.log(nacho.mascotas.length);
+// nacho.addbookNames("Rebelion en la granja", "George orwell")
+// nacho.addbookNames("jiji", "jaja")
+// console.log(nacho);
+// nacho.getBooksName()
+
+
+// EJERCICIO 2 //
+
+const fs = require('fs')
+
+
+
+class Contenedor {
+    constructor(path){
+        this.path = path
     }
+
+    async save(title, precio, img){
+        
+        try{
+            let id 
+            let productos = await fs.promises.readFile(this.path, 'utf-8' )
+            productos = JSON.parse(productos)
+            if (productos.length == 0){
+                id = 1
+            } else {
+                id = productos[productos.length - 1].id + 1
+            }
+
+            productos.push({id: id, title: title, precio: precio, img:img})
+
+            await fs.promises.writeFile(this.path, JSON.stringify(productos))
+
+            console.log('producto guardado', id);
+            
+            return id
+        }catch(err) {
+            console.log(`error al guardar produto   error:${err}`);
+        }
+    } 
 }
 
-const nacho = new Persona("Ignacio" , "dieguez" ,[], ["cherry" , "ginger"])
+const productos = new Contenedor('contenedor.json')
 
-console.log(nacho);
-nacho.getFullName()
-nacho.countMascotas
-console.log(nacho.mascotas.length);
-nacho.addbookNames("Rebelion en la granja", "George orwell")
-nacho.addbookNames("jiji", "jaja")
-console.log(nacho);
-nacho.getBooksName()
+productos.save('sds', 20, 'img')
