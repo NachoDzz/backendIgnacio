d703ec7a4ab1b82c6314a0740dbfaee7b098ae3b
Ignacio <ignaciodieguez@hotmail.com>
2022-06-09 15:37:30 -0300

    aaa

--- back.js
+++ back.js
@@ -47,37 +47,88 @@
 const fs = require('fs')
 
 
-
 class Contenedor {
-    constructor(path){
-        this.path = path
-    }
+    constructor(archivo) { 
+        this.archivo = archivo;
+            try { this.productos = fs.readFileSync(this.archivo, "utf-8");
+                this.productos = JSON.parse(this.productos); 
+            } catch (error) { 
+                this.productos = []; 
+            } }
 
-    async save(title, precio, img){
-        
-        try{
-            let id 
-            let productos = await fs.promises.readFile(this.path, 'utf-8' )
-            productos = JSON.parse(productos)
-            if (productos.length == 0){
-                id = 1
-            } else {
-                id = productos[productos.length - 1].id + 1
+            async save(title, price, url){
+                    try {
+                            let newProduct = { title, price, url };
+                                newProduct.id = this.productos.length + 1;
+                                    this.productos.push(newProduct);
+                                        await fs.promises.writeFile(
+                                                this.archivo,
+                                                    JSON.stringify(this.productos, null, "\t")
+                                                        );
+                    } catch (error) {
+                            console.log(error);
+                        }}
+
+            async getById(id){
+                try{
+                    let productos = await fs.promises.readFile(this.archivo, 'utf-8')
+                    productos = JSON.parse(productos)
+                    // console.log(productos);
+                    let producto = productos.find(prod => prod.id == id)
+                    console.log(producto.title);
+                    return producto.title
+                }catch(err){
+                    console.log(`no se encuentra el id o hubo un error ${err}`);
+                }
             }
 
-            productos.push({id: id, title: title, precio: precio, img:img})
+            async getAll(){
+                try{
+                    let productos = await fs.promises.readFile(this.archivo, 'utf-8')
+                    productos = JSON.parse(productos)
+                    
+                    console.log(productos);
+                    return productos
+                }catch (err){
+                    console.log('D:');
+                }
+            }
 
-            await fs.promises.writeFile(this.path, JSON.stringify(productos))
+            async deleById(id) {
+                try {
+                    let productos = await fs.promises.readFile(this.archivo, 'utf-8')
+                    productos = JSON.parse(productos)
+                    productos = productos.filter(prod => prod.id !== id)
+    
+                    await fs.promises.writeFile(this.path, JSON.stringify(productos))
+    
+                    console.log('producto elimina2', productos);
+    
+                    return productos
+                }catch(err){
+                    console.log(`el error es ${err}`);
+                }
+            }
 
-            console.log('producto guardado', id);
-            
-            return id
-        }catch(err) {
-            console.log(`error al guardar produto   error:${err}`);
-        }
-    } 
+            async deleAll() {
+                try {
+                    await fs.promises.writeFile(this.archivo, '[]')
+                    console.log('elimina2');
+                }catch(err){
+                    console.log(`D: ${err}`);
+                }
+            }
+
 }
 
-const productos = new Contenedor('contenedor.json')
+productos = new Contenedor('back.json')
 
-productos.save('sds', 20, 'img')
+// productos.save('birrita','250', 'img')
+productos.save('cigarro', 140, 'img2' )
+// productos.getById(1)
+// productos.getAll()
+// productos.deleById(1)
+// productos.deleAll()
+
+
+
